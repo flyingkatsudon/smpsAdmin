ec49a09b5cc1f73153f3b1bc9a7f557bcd8090a4
Jeremy371 <milky0610.mk@gmail.com>
2016-05-19 14:03:45 +0900

    excel object mapping

---
+++ src/main/java/com/blogspot/na5cent/exom/annotation/Column.java
@@ -0,0 +1,22 @@
+/*
+ * code https://github.com/jittagornp/excel-object-mapping
+ */
+package com.blogspot.na5cent.exom.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * @author redcrow
+ */
+@Target(ElementType.FIELD)
+@Retention(RUNTIME)
+public @interface Column {
+
+    String name() default "";
+
+    String pattern() default "";
+}
