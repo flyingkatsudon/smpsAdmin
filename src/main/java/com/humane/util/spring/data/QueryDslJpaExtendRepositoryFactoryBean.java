dc11309d24784044ea205f650883531654840aa6
MuzEye <muzeye@gmail.com>
2017-04-18 21:49:58 +0900

    .

--- src/main/java/com/humane/util/spring/data/QueryDslJpaExtendRepositoryFactoryBean.java
+++ src/main/java/com/humane/util/spring/data/QueryDslJpaExtendRepositoryFactoryBean.java
@@ -11,6 +11,10 @@ import java.io.Serializable;
 
 public class QueryDslJpaExtendRepositoryFactoryBean<R extends JpaRepository<T, I>, T, I extends Serializable> extends JpaRepositoryFactoryBean<R, T, I> {
 
+    public QueryDslJpaExtendRepositoryFactoryBean(Class<? extends R> repositoryInterface) {
+		super(repositoryInterface);
+	}
+
     protected RepositoryFactorySupport createRepositoryFactory(EntityManager entityManager) {
         return new CustomQueryDslJpaRepositoryFactory(entityManager);
     }
