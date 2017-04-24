36fb4e68549bdcca54a595eb350d27e173a00948
muzeye <muzeye@gmail.com>
2017-04-24 15:40:07 +0900

    .

--- src/test/java/com/humane/smps/ApplicationTests.java
+++ src/test/java/com/humane/smps/ApplicationTests.java
@@ -2,15 +2,14 @@ package com.humane.smps;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringRunner.class)
-@WebAppConfiguration
+@SpringBootTest
 public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
